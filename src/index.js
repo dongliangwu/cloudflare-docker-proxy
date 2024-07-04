1dbd1bfee3dead270ea206d56f50adb1d56336a2
dongliangwu <37110439+dongliangwu@users.noreply.github.com>
2024-07-04 17:06:04 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.dongliangwu123.top": dockerHub,
+  "quay.dongliangwu123.top": "https://quay.io",
+  "gcr.dongliangwu123.top": "https://gcr.io",
+  "k8s-gcr.dongliangwu123.top": "https://k8s.gcr.io",
+  "k8s.dongliangwu123.top": "https://registry.k8s.io",
+  "ghcr.dongliangwu123.top": "https://ghcr.io",
+  "cloudsmith.dongliangwu123.top": "https://docker.cloudsmith.io",
+  "ecr.dongliangwu123.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.dongliangwu123.top": dockerHub,
 };
 
 function routeByHosts(host) {
